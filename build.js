ae33acfaa47fb0fba0ece0d061a358eb0ff4ae75
Zetlen <james_zetlen@volusion.com>
2015-03-29 15:55:04 -0500

    removing excess baggage

--- build.js
+++ build.js
@@ -7,8 +7,6 @@ getNugetPackage(pkg.nuget).then(function(stream) {
   stream.pipe(unzip.Parse())
     .on('entry', function(entry) {
       var fileName = entry.path;
-      var type = entry.type; // 'Directory' or 'File'
-      var size = entry.size;
       if (fileName === "tools/ActionDefinitions.json") {
         console.log('Extracting action definitions...')
         entry.pipe(fs.createWriteStream('./data/action-definitions.json'));
