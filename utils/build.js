676d50717452c13a835c3ed515590fa1d055061e
Zetlen <james_zetlen@volusion.com>
2015-06-09 12:15:41 -0500

    updating build script

--- utils/build.js
+++ utils/build.js
@@ -1,10 +1,25 @@
 var getNugetPackage = require('./get-nuget-package');
 var unzip = require('unzip');
-var exec = require('child_process').exec;
+var childProcess = require('child_process');
 var fs = require('fs');
 
 var actionDefPath = './data/action-definitions.json';
 var packageActionDefPath = 'tools/ActionDefinitions.json';
+
+function exec(cmd) {
+  return new Promise(function(resolve, reject) {
+    childProcess.exec(cmd, {
+      cwd: './'
+    }, function(err, stdout, stderr) {
+      console.log(stderr);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
 
 getNugetPackage({
   host: process.env.npm_package_config_nugetHost,
@@ -17,16 +32,23 @@ getNugetPackage({
         console.log('Extracting action definitions...')
         entry.pipe(fs.createWriteStream(actionDefPath)).on('close', function() {
           console.log('Action definitions extracted to ' + actionDefPath);
-          exec('git add ' + actionDefPath + ' && git commit -m "Updating action definitions for Mozu API version ' + result.version + '" && npm version ' + result.version, {
-            cwd: './'
-          }, function(err, stdout, stderr) {
-            if (err) {
-              return console.error('Update failed! ' + err.message);
-            }
-            console.log(stdout);
-            console.log(stderr);
-            console.log('\nUpdate complete!');
-          })
+
+          exec('git ls-files -m')
+            .then(function(out) {
+              if (out.indexOf('action-definitions.json') === -1) {
+                throw new Error("No change to action definitions!");
+              } else {
+                return exec('git add ' + actionDefPath)
+              }
+            }).then(function() {
+              return exec('git commit -m "Updating action definitions for Mozu API version ' + result.version + '"');
+            }).then(function() {
+              return exec('npm version ' + result.version);
+            }).then(function(out) {
+              console.log(out);
+              console.log('\nUpdate complete! Run `npm publish` now to publish to the npm registry.')
+            }).catch(console.error);
+
         })
       } else {
         entry.autodrain();
