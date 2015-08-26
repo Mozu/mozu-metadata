8e9f16de0cd17826b943531d6f65e44faf9cf126
Zetlen <james_zetlen@volusion.com>
2015-08-26 16:22:54 -0500

    legacy compat

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  actionDefinitions: require('./data/action-definitions.json'),
   environments: require('./data/environments.json'),
   themes: require('./data/themes.json'),
   getActionDefinitions: require('./utils/get-action-defs.js')
